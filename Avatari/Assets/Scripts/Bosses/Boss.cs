67dccff3e3f24b8e2969b76931d954035131d186
Denholm Scrimshaw <scrimshaw_is_at@hotmail.com>
2016-07-13 02:13:28 -0700

    load in bosses from cache

--- Avatari/Assets/Scripts/Bosses/Boss.cs
+++ Avatari/Assets/Scripts/Bosses/Boss.cs
@@ -2,6 +2,7 @@
 
 public class Boss {
     private string bossName;
+    private string spriteName;
     private string requirement;
     private string info;
     private bool locked;
@@ -10,10 +11,27 @@ public class Boss {
    
     public Boss (string name, string info, int level, int bossID) {
         this.bossName = name;
+        this.spriteName = "monster" + bossID;
         this.info = info;
         this.bossID = bossID;
         this.level = level;
         this.requirement = "Reach level " + this.level;
         this.locked = true;
+    }
+
+    public string getSpriteName () {
+        return this.spriteName;
+    }
+
+    public string getName() {
+        return this.bossName;
+    }
+
+    public string getRequirement() {
+        return this.requirement;
+    }
+
+    public string getInfo() {
+        return this.info;
     }
 }
---
+++ Avatari/Assets/Scripts/Bosses/BossLoader.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections.Generic;
+
+public class BossLoader : MonoBehaviour {
+
+    private Cache cache;
+    private GameObject panelPrefab;
+    private Transform panelSpawner;
+    private const float rowVertOffset = 60.0f;
+
+    private void Awake () {
+        Initialize();
+        LoadBosses();
+    }
+
+    private void Initialize() {
+        cache = Utility.LoadObject<Cache>("Cache");
+        panelPrefab = Resources.Load<GameObject>("Prefabs/UI/Bosses/BossPanel");
+        panelSpawner = Utility.LoadObject<Transform>("BossPanelSpawner");
+    }
+
+    private void LoadBosses() {
+        List<Boss> bosses = this.cache.LoadBosses();
+        Vector3 offset = Vector3.down * rowVertOffset;
+
+        int i = 0;
+        foreach (var boss in bosses) {
+            GameObject clone = (GameObject)Instantiate(panelPrefab, 
+                panelSpawner.position+(offset*i++), Quaternion.identity);
+            clone.transform.SetParent(panelSpawner);
+
+            Text bossTitle = clone.transform.GetChild(0).GetComponent<Text>();
+            bossTitle.text = boss.getName();
+
+            Text bossRequirement = clone.transform.GetChild(1).GetComponent<Text>();
+            bossRequirement.text = boss.getRequirement();
+
+            Image bossIcon = clone.transform.GetChild(2).GetComponent<Image>();
+            bossIcon.sprite = cache.LoadBossSprite(boss.getSpriteName());
+
+            Image bossInfo = clone.transform.GetChild(3).GetComponent<Image>();
+            bossInfo.sprite = cache.LoadUtilitySprite("info");
+        }
+        
+    }
+}
