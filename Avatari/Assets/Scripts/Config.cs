a22aa037e55b87882e1b67f7de9cf10a7a596236
MisterPotter <tjpotter@uvic.ca>
2016-07-16 14:47:20 -0700

    Hitting account create and login end points

---
+++ Avatari/Assets/Scripts/Config.cs
@@ -0,0 +1,7 @@
+﻿public class Config {
+    public static readonly string ControllerURLRoot = "http://ec2-54-200-193-115.us-west-2.compute.amazonaws.com/app_dev.php/";
+    public static readonly string ControllerURLAccounts = ControllerURLRoot + "account/create";
+    public static readonly string ControllerURLLogin = ControllerURLRoot + "login";
+    public static readonly string ControllerURLOAuth = ControllerURLRoot + "oauth";
+    public static readonly string Token = "839uejdmi12ke9012ik1dpakpkdj30e2";
+}
---
+++ Avatari/Assets/Scripts/Login/LoginUIHandler.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class LoginUIHandler : MonoBehaviour {
+
+    private Text avatarName;
+
+    private void Awake() {
+        avatarName = Utility.LoadObject<Text>("AvatarInput");
+        WWW www = new WWW(Config.ControllerURLRoot);
+        StartCoroutine(WaitForRequest(www));
+    }
+
+    public void Login() {
+        Debug.Log(avatarName.text);
+
+        // Post our account
+        WWWForm accountsForm = new WWWForm();
+        accountsForm.AddField("token", Config.Token);
+        accountsForm.AddField("avatar_name", avatarName.text);
+        WWW accountWWW = new WWW(Config.ControllerURLAccounts, accountsForm);
+        StartCoroutine(WaitForRequest(accountWWW));
+
+        // Login 
+        WWWForm loginForm = new WWWForm();
+        loginForm.AddField("token", Config.Token);
+        WWW loginWWW = new WWW(Config.ControllerURLLogin, loginForm);
+
+        // Open up tab
+        Application.OpenURL(Config.ControllerURLOAuth);
+    }
+
+    IEnumerator WaitForRequest(WWW www) {
+        yield return www;
+
+        if (www.error == null) {
+            Debug.Log("WWW Ok: " + www.text);
+        } else {
+            Debug.Log("WWW Error: " + www.error);
+        }
+    }
+
+}
