73fb0b3a738bcaa696df9ac0c7712b57bd1c59da
CFedderly <cfed182@hotmail.com>
2016-07-17 13:46:27 -0700

    Now have a popup that opens the first time you visit the home screen to show stats updates. Right now doesn't actually reflect fitbit data

---
+++ Avatari/Assets/Scripts/Home/LoadStatsUpdate.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class LoadStatsUpdate : MonoBehaviour {
+
+    private static LoadStatsUpdate disIsYou;
+
+    private PlayerStatistic initStats;
+    private PlayerStatistic updatedStats;
+
+    private Cache cache;
+    private Transform dialogSpawner;
+
+    private const string prefabPath = "Prefabs/UI/StatisticsPopup/StatsDialog";
+
+	void Start () {
+        if (disIsYou == null) {
+            Initialize();
+        } else if (disIsYou != this) {
+            Destroy(this.gameObject);
+        }
+	}
+
+    private void Initialize() {
+        DontDestroyOnLoad(gameObject);
+        disIsYou = this;
+
+        cache = Utility.LoadObject<Cache>("Cache");
+        dialogSpawner = Utility.LoadObject<Transform>("DialogSpawner");
+
+        initStats = this.cache.player.stats;
+
+        this.cache.player.stats.UpdatePlayerStatistics();
+        updatedStats = this.cache.player.stats;
+        CleanUpExistingDialogs();
+        CreateStatsDialog();
+    }
+
+    private void CleanUpExistingDialogs() {
+        foreach (Transform dialog in this.dialogSpawner) {
+            Destroy(dialog.gameObject);
+        }
+    }
+
+    private void CreateStatsDialog() {
+        GameObject prefab = Resources.Load<GameObject>(prefabPath);
+        if (prefab == null) {
+            throw new System.Exception("Statistics dialog prefab not found.");
+        }
+        GameObject dialog = (GameObject)Instantiate(prefab, new Vector2(0.0f, 0.0f), Quaternion.identity);
+
+        Button confirm = dialog.transform.Find("Confirm").GetComponent<Button>();
+
+        confirm.onClick.AddListener(
+            delegate {
+                Destroy(dialog);
+            });
+
+        dialog.transform.SetParent(dialogSpawner, false);
+    }
+	
+}
