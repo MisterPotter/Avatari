45b147e7bacf4cf238c6a8c852aaec5b5234c4f9
MisterPotter <tjpotter@uvic.ca>
2016-07-19 11:36:30 -0700

    working on game controller

---
+++ Avatari/Assets/Scripts/Battle/BattleController.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+public class BattleController : MonoBehaviour {
+
+    private bool turnStarted;
+
+    private PlayerStatistic boss;
+    private Cache cache;
+    private PlayerStatistic player;
+    
+
+    private void Awake() {
+        Initialize();
+    }
+
+    private void Initialize() {
+        this.turnStarted = false;
+        this.cache = Utility.LoadObject<Cache>("Cache");
+        this.player = this.cache.player
+    }
+
+    public void Attack() {
+        if(!turnStarted) {
+            turnStarted = true;
+            Debug.Log("Attack");
+            turnStarted = false;
+        }
+    }
+
+    public void Flee() {
+        if (!turnStarted) {
+            turnStarted = true;
+            Debug.Log("Flee");
+            turnStarted = false;
+        }
+    }
+}
